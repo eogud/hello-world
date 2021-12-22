b8b51da4ffb9f2ca84d81370de087ba25460d21d
eogud <eogud@DESKTOP-HPISBIT>
2021-12-23 01:11:25 +0900

    변경된 코드가 git에 올라갔는지 테스트

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello 열심히하자");
 		System.out.println("띄워쓰기는 자동으로 되는가 ");
+		System.out.println("변경된 코드는 올라갔는가 ");
+	
 	}
 
 }
