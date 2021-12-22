75d1175666300567969e404355b305b1b40a049b
eogud <eogud@DESKTOP-HPISBIT>
2021-12-23 01:17:29 +0900

    깃에 제대로 올라갔니

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello 열심히하자");
 		System.out.println("띄워쓰기는 자동으로 되는가 ");
-		System.out.println("변경된 코드는 올라갔는가 ");
-	
+		System.out.println("변경된 코드는 올라갔는가");
+		System.out.println("변경된 코드는 올라갔는가");
+		System.out.println("변경된 코드는 올라갔는가");
+		System.out.println("변경된 바바바 올라갔는가");
+		System.out.println("변경된 코드는 올라갔는가");
+		
 	}
 
 }
+
